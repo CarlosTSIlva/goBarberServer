e868c24c1fd92088539ac4fa5387d6c9267fcbb2
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-07-29 18:51:04 -0300

    Route create

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+import {  startOfHour, parseISO, isEqual } from 'date-fns'
+import Appointment from '../models/Appointment'
+
+const appointmentsRouter = Router()
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (req,res) => {
+  const { provider, date } = req.body
+
+  const parsedDate = startOfHour(parseISO(date))
+  const findAppointmentInSameDate = appointments.find(appointment =>
+     isEqual(parsedDate, appointment.date),
+    );
+    if (findAppointmentInSameDate){
+      return res.status(400).json({ message: ' This appoinment is already booked' })
+    }
+      const appointment = new Appointment(provider,parsedDate)
+  appointments.push(appointment)
+
+  return res.json(appointment);
+})
+
+
+
+
+export default appointmentsRouter
