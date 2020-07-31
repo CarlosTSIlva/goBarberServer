261db5058e7c267e66626cfed6f9545ee97ed8e3
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-07-31 12:49:37 -0300

    listen

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,33 @@
-import { Router } from 'express'
-import {  startOfHour, parseISO, isEqual } from 'date-fns'
-import Appointment from '../models/Appointment'
+import { Router } from "express";
+import { startOfHour, parseISO } from "date-fns";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
-const appointmentsRouter = Router()
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments: Appointment[] = [];
+appointmentsRouter.get("/", (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.json(appointments);
+});
 
-appointmentsRouter.post('/', (req,res) => {
-  const { provider, date } = req.body
+appointmentsRouter.post("/", (req, res) => {
+  const { provider, date } = req.body;
 
-  const parsedDate = startOfHour(parseISO(date))
-  const findAppointmentInSameDate = appointments.find(appointment =>
-     isEqual(parsedDate, appointment.date),
-    );
-    if (findAppointmentInSameDate){
-      return res.status(400).json({ message: ' This appoinment is already booked' })
-    }
-      const appointment = new Appointment(provider,parsedDate)
-  appointments.push(appointment)
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: " This appointment is already booked" });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
-})
+});
 
-
-
-
-export default appointmentsRouter
+export default appointmentsRouter;
